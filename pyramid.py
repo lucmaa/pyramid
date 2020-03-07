efdd80b0b8ceee74ea84b8ee41846081cf518d10
luc <lucmaa@qq.com>
2020-03-07 22:41:47 +0800

    feat: parse caller and callee pairs

--- pyramid.py
+++ pyramid.py
@@ -2,27 +2,46 @@
 import re
 import sys
 
-calls = []
+rtl = []
 
 
-class CallNode(object):
-    """ A struct to denote the node and edge of a call. It contains a caller and callee.
-    """
+class Calls(object):
+    """ A struct to denote the node and edge of a call. It contains a caller and callee. """
+    def __repr__(self):
+        return '\n'.join(self.__dict__.keys())
 
 
-def parse_rtl():
-    for rtl_line in calls:
+class CalleeHolder(object):
+    """ A class holds a callee so that the callee is a NoneType"""
+
+
+def parse_rtl(calls):
+    caller_keeper = ''
+    for rtl_line in rtl:
         caller = re.match(r'^;; Function (.*)\s+\((.*)?\)$', rtl_line)
-        if caller is None:
+        if caller is not None:
+            caller_keeper = caller.group(1)
+            if not hasattr(calls, caller_keeper):
+                setattr(calls, caller_keeper, CalleeHolder())
             continue
-        print(caller.group(1))
+
+        callee = re.match(r'^.*\(call.*"(.*)".*$', rtl_line)
+        if callee is not None:
+            _callee = callee.group(1)
+            setattr(getattr(calls, caller_keeper), _callee, 'call')
+            continue
+
+        ref = re.match(r'^.*\(symbol_ref.*"(.*)".*$', rtl_line)
+        if ref is not None:
+            _ref = ref.group(1)
+            setattr(getattr(calls, caller_keeper), _ref, 'ref')
 
 
 def load_rtl_files(files):
     for f_path in files:
         with open(f_path) as f:
             for line in f:
-                calls.append(line.strip())
+                rtl.append(line.strip())
 
 
 def main(argv=None):
@@ -41,4 +60,6 @@ def main(argv=None):
 
 if __name__ == '__main__':
     main()
-    parse_rtl()
+    c = Calls()
+    parse_rtl(c)
+    print(c)
