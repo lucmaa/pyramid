ccf99333d723b438184572f598a4051f533bc9db
luc <lucmaa@qq.com>
2020-03-08 11:36:59 +0800

    feat: encapsulate Application class Pyramid
    Now the work of rewriting from Egypt to Pyramid is done. One can use it
    like
    
    $python pyramid.py /path/to/rtl-files | dot -Gsize=100,100 -Tsvg -o
    callgraph.svg

--- pyramid.py
+++ pyramid.py
@@ -2,46 +2,72 @@
 import re
 import sys
 
-rtl = []
+
+class Pyramid(object):
+    """The application class is responsible for input/output and contains a rtl parser"""
+
+    def __init__(self, rtl_files):
+        self.calls = Calls()
+        self.rtl = []
+        for f_path in rtl_files:
+            with open(f_path) as f:
+                for line in f:
+                    self.rtl.append(line.strip())
+
+    def parse_rtl(self):
+        caller_keeper = ''
+        for rtl_line in self.rtl:
+            caller = re.match(r'^;; Function (.*)\s+\((.*)?\)$', rtl_line)
+            if caller is not None:
+                caller_keeper = caller.group(1)
+                if not hasattr(self.calls, caller_keeper):
+                    setattr(self.calls, caller_keeper, CallerHolder())
+                continue
+
+            callee = re.match(r'^.*\(call.*"(.*)".*$', rtl_line)
+            if callee is not None:
+                _callee = callee.group(1)
+                _caller = getattr(self.calls, caller_keeper)
+                setattr(_caller, _callee, 'call')
+                setattr(_caller, 'is_caller', True)
+                continue
+
+            ref = re.match(r'^.*\(symbol_ref.*"(.*)".*$', rtl_line)
+            if ref is not None:
+                _ref = ref.group(1)
+                setattr(getattr(self.calls, caller_keeper), _ref, 'ref')
+
+    def run(self):
+        self.parse_rtl()
+        print(self.calls)
 
 
 class Calls(object):
-    """ A struct to denote the node and edge of a call. It contains a caller and callee. """
+    """ A tree to denote the caller and callee pairs. """
     def __repr__(self):
-        return '\n'.join(self.__dict__.keys())
+        digraph = ['digraph callgraph {']
+
+        for _caller in self.__dict__.keys():
+            _caller_obj = getattr(self, _caller)
+            if not getattr(_caller_obj, 'is_caller'):
+                continue
+            delattr(_caller_obj, 'is_caller')
+            for _callee in _caller_obj.__dict__.keys():
+                # check if the callee is already defined in this scope
+                if getattr(_caller_obj, _callee) == 'ref' or not hasattr(self, _callee):
+                    continue
+                digraph.append(f'"{_caller}" -> "{_callee}" [style=solid];')
+
+        digraph.append('}')
+
+        return '\n'.join(digraph)
 
 
-class CalleeHolder(object):
-    """ A class holds a callee so that the callee is a NoneType"""
+class CallerHolder(object):
+    """ A class holds a caller in case that the caller is a NoneType"""
 
-
-def parse_rtl(calls):
-    caller_keeper = ''
-    for rtl_line in rtl:
-        caller = re.match(r'^;; Function (.*)\s+\((.*)?\)$', rtl_line)
-        if caller is not None:
-            caller_keeper = caller.group(1)
-            if not hasattr(calls, caller_keeper):
-                setattr(calls, caller_keeper, CalleeHolder())
-            continue
-
-        callee = re.match(r'^.*\(call.*"(.*)".*$', rtl_line)
-        if callee is not None:
-            _callee = callee.group(1)
-            setattr(getattr(calls, caller_keeper), _callee, 'call')
-            continue
-
-        ref = re.match(r'^.*\(symbol_ref.*"(.*)".*$', rtl_line)
-        if ref is not None:
-            _ref = ref.group(1)
-            setattr(getattr(calls, caller_keeper), _ref, 'ref')
-
-
-def load_rtl_files(files):
-    for f_path in files:
-        with open(f_path) as f:
-            for line in f:
-                rtl.append(line.strip())
+    def __init__(self, is_caller=False):
+        self.is_caller = is_caller
 
 
 def main(argv=None):
@@ -50,7 +76,7 @@ def main(argv=None):
         argv = sys.argv
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'vo:')
-        load_rtl_files(args)
+        Pyramid(args).run()
     except getopt.error as msg:
         sys.stdout = sys.stderr
         print(msg)
@@ -60,6 +86,3 @@ def main(argv=None):
 
 if __name__ == '__main__':
     main()
-    c = Calls()
-    parse_rtl(c)
-    print(c)
